bdb5184fb5f7a062f6b58fcab930576ce2b739d2
D.G.C Anupama <chamathanupama3@gmail.com>
2025-09-22 14:48:31 +0530

    User input method.

--- src/User.java
+++ src/User.java
@@ -1,6 +1,9 @@
-
+import java.util.Scanner;
 public class User {
     public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        String name = x.nextLine();
+        System.out.println(name);
 
     }
 }
