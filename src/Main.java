77abb8636ea6ffcda9cf73a239849d70776210fa
D.G.C Anupama <chamathanupama3@gmail.com>
2025-09-22 14:08:36 +0530

    Added two print statements.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //write anything here.
+        System.out.println("Hello Wrold");
+        System.out.println("My name is Chamath Anupama");
     }
 }
